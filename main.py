5df38ee7f9ab58b5e751792c8b43903ffc34ee25
toomas <toimik007@hotmail.com>
2021-11-24 19:44:18 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#korduste arv:
+kordusi = 10
+#
+ret = 0
+#
+for nr in range(kordusi):#
+    print(nr)#
+    jrk = nr+1#
+    #järgmine rida teeb mida?
+    val = input("Sisesta "+str(jrk)+". number: ")
+    # kuna input annab meile tekstilise muutuja, siis peame koverteerima 
+    val = int(val)
+    if val > 1 or val < 100:
+            ret = ret+val
+            print(ret)
+ret = ret/kordusi
+print("Antud arvude keskmine on: "+str(ret))
